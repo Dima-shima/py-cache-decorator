3bf5ba440d7efe3649c5d9c8aa02b4a2e3c9ac90
Dima-shima <shifrin1970@gmail.com>
2026-05-04 13:44:25 +0300

    implement py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args, **kwargs):
+        nonlocal cache_dict
+        if args in cache_dict.keys():
+            result = cache_dict[args]
+            print("Getting from cache")
+        else:
+            result = (func(*args, **kwargs))
+            print("Calculating new result")
+        cache_dict[args] = result
+        return result
+    return inner
